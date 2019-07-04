272fe59a1a6c585a16ff36819499ffcb0c4f0d03
Leo Scarano <leoscarano6@gmail.com>
2019-07-04 10:59:34 -0400

    Uploading project files

---
+++ src/main/java/com/pino/db/pino/PinoApplication.java
@@ -0,0 +1,12 @@
+package com.pino.db.pino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinoApplication.class, args);
+	}
+}
